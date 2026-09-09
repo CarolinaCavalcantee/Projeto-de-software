b75ec4c0bd4db803f94e819ae90893010874a8ba
Carolina Cavalcante <accj@ic.ufal.br>
2026-09-09 08:57:45 -0300

    Refactor painel.py to use dictionaries for data storage

--- servicos/painel.py
+++ servicos/painel.py
@@ -1,28 +1,36 @@
-# aqui a gnt vai fzr um formato que, sem as apis falharem, a gnt tem alguma coisa ainda
+import logging
 def montar_painel_completo():
     dados_painel = {
-        "voos": [],
-        "onibus_e_trens": [],
+        "voos": {},
+        "onibus_e_trens": {},
         "avisos": []
     }
 
-    #primeiro vms tentar achar cada api separada
     try:
         voo = buscar_voo()
         if voo:
-            dados_painel["voos"].append(voo.exibir_status())
+            dados_painel["voos"][voo.numero_voo] = voo.exibir_status()
+
     except Exception as e:
-        print(f"Erro ao carregar voo {e}")
-        dados_painel["avisos"].append("Dados de voos temporariamente indisponíveis.")
+        logging.error(f"Erro ao buscar voo: {e}")
+        dados_painel["avisos"].append("Dados temporariamente indisponíveis.")
 
     try:
         dados_gtfs = buscar_gtfs_api()
         transportes = processar_dados_gtfs(dados_gtfs)
+
         for transporte in transportes:
-            dados_painel["onibus_e_trens"].append(transporte.exibir_status())
-            
+            if hasattr(transporte, 'trip_id'):
+                id_unico = transporte.trip_id
+            elif hasattr(transporte, 'linha_onibus'):
+                id_unico = transporte.linha_onibus
+            else:
+                id_unico = transporte.linha_trem
+
+            dados_painel["onibus_e_trens"][id_unico] = transporte.exibir_status()
+
     except Exception as e:
-        print(f"Erro ao carregar ônibus/trem {e}")
-        dados_painel["avisos"].append("Dados de transportes terrestres temporariamente indisponíveis.")
+        logging.error(f"Erro ao buscar ônibus/trem: {e}")
+        dados_painel["avisos"].append("Dados temporariamente indisponíveis.")
 
     return dados_painel
